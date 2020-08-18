1b4ed2e413ff409828e6988b32976e0f032b3dde
perzxf <963726646@qq.com>
2020-08-18 09:44:52 +0800

    提交热部署

--- src/main/java/com/ls/VLoushiApplication.java
+++ src/main/java/com/ls/VLoushiApplication.java
@@ -2,7 +2,13 @@ package com.ls;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 热更新、热加载
+ * 1、Settings->Compiler->Build project automatically
+ * 2、Ctrl+Shift+A ->搜索registry，找到Registry...，注意是后面有三个点的那个，然后找到compiler.automake.allow.when.app.running，勾选
+ * 3、热部署快捷键Ctrl+F9,java代码修改后需要执行一下快捷键才能实际生效
+ *
+ */
 @SpringBootApplication
 public class VLoushiApplication {
 
