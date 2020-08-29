d00394c277fcdd11147748b1735af28ff94a97e7
perzxf <963726646@qq.com>
2020-08-29 17:49:02 +0800

    提交常量类（zcf-2020-08-29）

--- src/main/java/com/ls/common/ConstantConfig.java
+++ src/main/java/com/ls/common/ConstantConfig.java
@@ -13,6 +13,7 @@ public class ConstantConfig {
      * 是否被封禁（1：未封禁，0：已封禁）
      */
     public final static Integer ISOFF = 1;
+    public final static Integer NOOFF = 0;
 
     /**
      * 使用结束时间（新用户有3天使用时间，如需延长找管理员开通）
