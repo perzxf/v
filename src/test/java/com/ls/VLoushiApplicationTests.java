7454d5a643e75a6fae696ba4c430ee2be90a529a
cx <cx525606@gmail.com>
2020-08-31 12:24:22 +0800

    提交网站管理添加和删除（ct-2020-8-31）

--- src/test/java/com/ls/VLoushiApplicationTests.java
+++ src/test/java/com/ls/VLoushiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.ls;
 
+
+
 import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class VLoushiApplicationTests {
 
+
     @Test
     void contextLoads() {
     }
+
+
+
 
 }
