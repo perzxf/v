c4d434a1dabdcea8d5c05436e896090bddfb67f7
perzxf <963726646@qq.com>
2020-09-01 17:35:54 +0800

    提交微信公众号预警代码（zcf-2020-09-01）

---
+++ src/main/java/com/ls/mapper/weChat/WeChatUserInfoMapper.java
@@ -0,0 +1,7 @@
+package com.ls.mapper.weChat;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.ls.entity.weChat.WeChatUserInfo;
+
+public interface WeChatUserInfoMapper extends BaseMapper<WeChatUserInfo> {
+}
